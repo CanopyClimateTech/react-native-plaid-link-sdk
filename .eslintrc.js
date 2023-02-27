e2338c002ec3a51ed4f0a8a56dccb871395d5647
cantide5ga <cantide5ga@users.sourceforge.net>
2023-02-27 09:23:25 -0700

    troubleshooting compile errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,26 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      arrowFunctions: true,
+      jsx: true,
+      modules: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-uses-react': 2,
+    'react/jsx-uses-vars': 2,
+    'react/react-in-jsx-scope': 2,
+  },
 };
